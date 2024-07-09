26a2361aa0af6b7cf73cd676c86a8df99705035f
Umbra122 <byakuya209@gmail.com>
2024-07-09 10:47:35 -0700

    Added Base Class class for professions

---
+++ Scripts/Professions/BaseClass.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+using System.Transactions;
+
+public partial class BaseClass
+{
+    public string characterName;
+    public int attackPower;
+    public int defense;
+    public int health;
+    public int specialPower;
+    public int exp;
+    public int dex;
+    public int intelligence;
+    public int str;
+
+    public void Attack()
+    {
+
+    }
+    public void Defend()
+    {
+
+    }
+    public void RunAway()
+    {
+
+    }
+    public void UseItem()
+    {
+
+    }
+    public void SpecialAbility()
+    {
+
+    }
+}
